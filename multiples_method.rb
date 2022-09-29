9fcc7f9921f3d26472eb40b3beff750986568b4c
Johnstone Marucha <marucha.webdev@yahoo.com>
2022-09-29 09:43:21 +0300

    Add conditions to check negative numbers

--- multiples_method.rb
+++ multiples_method.rb
@@ -1,16 +1,20 @@
 def solution number 
-    passed = []
-    range = 0...number
-    sum = 0
+    if number < 0 
+        return 0 
+    else
+        passed = []
+        range = 0...number
+        sum = 0
 
-    puts "#{range}"
-    
-    range.each do |digit| 
-        if digit % 3 == 0 or digit % 5 == 0 
-            passed.push(digit)
-            puts "#{passed}"
+        puts "#{range}"
+        
+        range.each do |digit| 
+            if digit % 3 == 0 or digit % 5 == 0 
+                passed.push(digit)
+                puts "#{passed}"
+            end 
         end 
-    end 
 
-    return passed.reduce { |sum,num| sum + num } 
+        return passed.reduce { |sum,num| sum + num } 
+    end 
 end 
